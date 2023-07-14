24ac5fbbbe7594de0a3f01b6020dfdc43007ad8a
Kitwana Akil <kakil@me.com>
2023-07-14 19:55:04 -0400

    Completed List View Tutorial.

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,14 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    let people = ["Finn", "Leia", "Luke", "Rey"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        // List View
+        List {
+            Text("Static Row 1")
+            
+            ForEach(people, id: \.self) {
+                Text("\($0)")
+            }
+            Text("Static Row 2")
+//            Section("Section 1") {
+//                Text("Static Row 1")
+//                Text("Static Row 2")
+//            }
+//
+//
+//            Section("Section 2") {
+//                ForEach(0..<5) {
+//                    Text("Dynamic row \($0)")
+//                }
+//            }
+//
+//            Section("Section 3") {
+//                Text("Static Row 3")
+//                Text("Static Row 4")
+//            }
         }
-        .padding()
+        .listStyle(.grouped)
     }
 }
 
