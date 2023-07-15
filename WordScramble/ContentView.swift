654a1b2755c2427aa05777d5562131fc401a9f0a
Kitwana Akil <kakil@me.com>
2023-07-14 20:12:35 -0400

    Completed the Text API tutorial.

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -17,6 +17,34 @@ struct ContentView: View {
         }
         .padding()
     }
+    
+    func test() {
+//        let input = "a b c"
+//        let letters = input.components(separatedBy: " ")
+        
+        let input = """
+a
+b
+c
+"""
+        let letters = input.components(separatedBy: "\n")
+        let letter = letters.randomElement()
+        
+        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        
+        // This explores the use of the Objective-C method UITextChecker() and how to
+        // use it with Swift
+        let word = "swift"
+        let checker = UITextChecker()
+        
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        let allGood = misspelledRange.location == NSNotFound
+        
+        
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
